2bcc4aa5e64afe6f4bc7c482d4c0abb93af7f432
Maxboo <mbourrier@booprod.com>
2022-06-03 14:44:02 +0200

    Ajout objet retour pour ReservationMultiDestination

--- AgenceVoyage/AgenceVoyage/Controllers/ReservationMultiDestinationController.cs
+++ AgenceVoyage/AgenceVoyage/Controllers/ReservationMultiDestinationController.cs
@@ -21,23 +21,27 @@ namespace AgenceVoyage.Controllers
         }
 
         /// <summary>
-        /// Ajoute une réservation avec plusieurs trains
+        /// Ajoute une réservation avec plusieurs trains.
+        /// </ br> Chaque gare est sélectionné avec l'id de la ville.
         /// </summary>
         /// <param name="reservationMultiRequest">L'objet de réservation et les trains voulus</param>
         /// <returns>Un code 201 ou une erreur</returns>
         [HttpPut]
-        public IActionResult AjouterReservation([FromBody()] ReservationMultiRequest reservationMultiRequest)
+        public IActionResult AjouterReservation([FromBody()] Request_ReservationMulti reservationMultiRequest)
         {
             try
             {
                 // Vérification du nombre de destinations
                 if (reservationMultiRequest.villes.Count < 2)
-                    return BadRequest("Deux destinations minimum.");
+                    return BadRequest("Le nombre de ville doit être supérieur à 2.");
 
                 // Binding des destinations avec les gares
-                var destinations = new List<Gare>();
                 StringBuilder msgDestinations = new StringBuilder();
                 msgDestinations.AppendLine("Gares sélectionné : ");
+
+                #region Recherche des gares à atteindre selon les villes entrées.
+                // 500 : Renvoie la destinations introuvables à partir d'un des villes sélectionné.
+                var destinations = new List<Gare>();
                 foreach (Ville item in reservationMultiRequest.villes)
                 {
                     var Gares =
@@ -45,7 +49,7 @@ namespace AgenceVoyage.Controllers
                         join gdv in model.Garedessertville on v.IdVille equals gdv.IdVille
                         join g in model.Gare on gdv.IdGare equals g.IdGare
                         where
-                            v.NomVille == item.NomVille
+                            v.IdVille == item.IdVille
                         select g;
                     var destination = Gares.SingleOrDefault();
 
@@ -59,6 +63,7 @@ namespace AgenceVoyage.Controllers
 
                     destinations.Add(destination);
                 }
+                #endregion
 
                 // Recherche des correspondances avec les gares
                 var correspondances = new List<Train>();
@@ -81,13 +86,13 @@ namespace AgenceVoyage.Controllers
                 reservationModel.IdClient = reservationMultiRequest.idClient;
                 reservationModel.Confirme = false;
 
-
                 Reservation reservation = model.Reservation.Add(reservationModel).Entity;
                 model.SaveChanges();
 
                 // Ajout des réservations aux correspondances
                 StringBuilder msgReservationTrain = new StringBuilder();
                 msgReservationTrain.AppendLine("Réservation de train ajouté : ");
+                var reservationsTrain = new List<Trainreservation>();
                 foreach (var train in correspondances)
                 {
                     var trainReservModel = new Trainreservation();
@@ -95,6 +100,9 @@ namespace AgenceVoyage.Controllers
                     trainReservModel.IdTrain = train.IdTrain;
 
                     Trainreservation trainReserv = model.Trainreservation.Add(trainReservModel).Entity;
+                    model.SaveChanges();
+                    reservationsTrain.Add(trainReserv);
+
                     msgReservationTrain.AppendLine(" - Réservation du train n°" + train.IdTrain + " ajouté à la réservation n°" + trainReserv.IdReservation);
                 }
 
@@ -111,11 +119,19 @@ namespace AgenceVoyage.Controllers
                 msgResultat.AppendLine(" - Reservation ajouté au client n°" + reservationMultiRequest.idClient);
 
 
-                return Created("/reservationMultiDestination/", msgResultat.ToString());
+                Response_ReservationMultiResponse response = new Response_ReservationMultiResponse();
+                response.Trainreservation = reservationsTrain;
+                response.CorrespondancesTrain = correspondances;
+                response.Reservation = reservation;
+                response.Recapitulatif = msgResultat.ToString();
+                response.GareArret = destinations;
+
+                return Created("/reservationMultiDestination/", response);
             }
             catch (Exception e)
             {
-                throw e;
+                //throw e;
+                return BadRequest(e.Message);
             }
         }
     }
---
+++ AgenceVoyage/AgenceVoyage/ModelsRequest/Request_ReservationMulti.cs
@@ -0,0 +1,32 @@
+﻿using AgenceVoyage.Models;
+using System.Collections.Generic;
+
+namespace AgenceVoyage.ModelsRequest
+{
+
+    public class Request_ReservationMulti
+    {
+        public int idClient { get; set; }
+
+        public int nbrPassager { get; set; }
+
+        public List<Ville> villes { get; set; } = new List<Ville>();
+
+    }
+
+    public class Response_ReservationMultiResponse
+    {
+        public Reservation Reservation { get; set; }
+
+        public List<Train> CorrespondancesTrain { get; set; }
+
+        public List<Trainreservation> Trainreservation { get; set; }
+
+        public List<Gare> GareArret { get; set; }
+
+        public string Recapitulatif;
+    }
+
+
+
+}
