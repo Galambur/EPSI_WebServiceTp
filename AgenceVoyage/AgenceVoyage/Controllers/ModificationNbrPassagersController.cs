fc65560205acf2607b75e85c1df5f4f62e7056f4
Galambur <gaelle.derambure@epsi.fr>
2022-06-03 14:05:16 +0200

    Ajouté les codes de retour Modification

--- AgenceVoyage/AgenceVoyage/Controllers/ModificationNbrPassagersController.cs
+++ AgenceVoyage/AgenceVoyage/Controllers/ModificationNbrPassagersController.cs
@@ -24,22 +24,25 @@ namespace AgenceVoyage.Controllers
         /// </summary>
         /// <param name="idReservation">L'identifiant de la réservation à modifier</param>
         /// <param name="nbrPassagers">Le nouveau nommbre de passagers</param>
-        /// <returns>Un code 200 ou une erreur</returns>
+        /// <returns type="IActionResult">Un code 200 / 204 ou 500</returns>
         [HttpPost("{idReservation}")]
         public IActionResult ModifyNbrPassagers(int idReservation, [FromBody()] int nbrPassagers)
         {
+            if(nbrPassagers <= 0)
+                return BadRequest("Vous devez fournir un nombre de passagers supérieur à 0 dans le body");
+
             try
             {
                 var reservation = model.Reservation.SingleOrDefault(r => r.IdReservation == idReservation);
                 if (reservation == null || reservation == default(Reservation))
-                    return BadRequest("Identifiant de réservation introuvable.");
+                    return NoContent();
                 reservation.NbrPassager = nbrPassagers;
                 model.SaveChanges();
                 return Ok();
             }
             catch (Exception e)
             {
-                throw e;
+                return StatusCode(500);
             }
         }
     }
